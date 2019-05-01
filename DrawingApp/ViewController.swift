e7fd52cb1d3f063cd7547e6273f4afcb9ff342ca
Oleksandr Ignatenko <avi.cpp@gmail.com>
2019-05-01 22:54:39 +0200

    Initial Commit

---
+++ DrawingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Oleksandr Ignatenko on 01/05/2019.
+//  Copyright © 2019 Oleksandr Ignatenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
